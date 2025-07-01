598cbee0912f93a77060eb7a92335a1832af2a0f
ArduCrow <@proton.me>
2025-07-01 11:28:35 +0100

    add print statement

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from getter import get_data
 def main():
     url = "http://google.com"
     get_data.request_data(url)
+    print("Data fetched successfully.")
 
 if __name__ == '__main__':
     main()
